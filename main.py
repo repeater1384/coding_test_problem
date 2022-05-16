8888989fc23ac49f4cdde8ec9fbb518347c9ef84
ilkyu Park <repeater1384@naver.com>
2022-05-16 15:11:57 +0900

    0516 middle commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(1)
+T = input()
+for tc in ['#' + str(t) for t in range(1, T + 1)]:
+    n = int(input())
+    arr = [*map(int, input().split())]
+    answer = 0
+
+    for i in range(1, n - 1):
+        if arr[i] != min(arr[i - 1:i + 2]) and arr[i] != max(arr[i - 1:i + 2]):
+            answer += 1
+    print(tc, answer)
---
+++ sw_expert/Pr. 3.py
@@ -0,0 +1,29 @@
+def dfs(cur, depth, max_depth):
+    global answer
+
+    if depth == max_depth:
+        return
+    for idx in range(len(cur)):
+        
+
+
+T = int(input())
+for tc in ['#' + str(t) for t in range(1, T + 1)]:
+    arr, n = input().split()
+    arr, n = [*arr], int(n)
+    answer = 0
+    print(tc, answer)
+
+# 1206
+# T = 10
+# for tc in ['#' + str(t) for t in range(1, T + 1)]:
+#     n = int(input())
+#     arr = [*map(int, input().split())]
+#     answer = 0
+#
+#     for i in range(2, n - 2):
+#         cur = arr[i]
+#         left, right = max(arr[i - 2:i]), max(arr[i + 1:i + 3])
+#         if cur >= left and cur >= right:
+#             answer += min(cur - left, cur - right)
+#     print(tc, answer)
